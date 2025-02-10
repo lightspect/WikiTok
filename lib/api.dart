0bdf425304a7a32490e285cffa5f3149a512c7c4
Trinh Minh Duc <trinhducx@gmail.com>
2025-02-10 22:33:12 +0700

    update Api parameter map

--- lib/api.dart
+++ lib/api.dart
@@ -8,16 +8,31 @@ class ApiService {
 
   ApiService(this.baseUrl);
 
+  final queryParameter = {
+    'action': 'query',
+    'format': 'json',
+    'prop': 'extracts|pageimages|info',
+    'generator': 'random',
+    'formatversion': '2',
+    'exsentences': '5',
+    'exlimit': 'max',
+    'exintro': '1',
+    'explaintext': '1',
+    'pithumbsize': '400',
+    'inprop': 'url',
+    'grnnamespace': '0',
+    'grnlimit': '20'
+  };
+
   Future<Map<String, dynamic>> getRequest(String endpoint) async {
-    final url = Uri.parse('$baseUrl$endpoint');
+    final url = Uri.http(baseUrl, endpoint, queryParameter);
     final response = await http.get(url);
     return jsonDecode(response.body);
   }
 
   Future<List<WikiArticle>> getArticles() async {
-    final response = await getRequest(
-        '/api.php?action=query&format=json&prop=extracts%7Cpageimages%7Cinfo&generator=random&formatversion=2&exsentences=5&exlimit=max&exintro=1&explaintext=1&pithumbsize=400&inprop=url&grnnamespace=0&grnlimit=20');
-    final List<dynamic> articles = response['query']['pages'];
+    final response = await getRequest('/api.php');
+    final List<dynamic> articles = response['query']['search'];
     return articles.map((json) => WikiArticle.fromJson(json)).toList();
   }
 }
